4a95e045cae23969670e93830bccccec9289577c
Andrejs Cunskis <andrejs.cunskis@4finance.com>
2015-08-18 09:44:22 +0300

    Unused import removed

--- src/test/java/lv/ss/at/SsTest.java
+++ src/test/java/lv/ss/at/SsTest.java
@@ -13,7 +13,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
-import java.util.Collections;
 import java.util.List;
 
 import static org.hamcrest.CoreMatchers.is;
