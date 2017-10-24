5b51376c9c995856c905eb070661e332322089bc
Andrejs Cunskis <acunskis@evolutiongaming.com>
2017-10-24 11:22:18 +0300

    Added checkstyle

--- src/test/java/lv/ss/at/selenium/pages/wrappers/AdvertisementItemWrapper.java
+++ src/test/java/lv/ss/at/selenium/pages/wrappers/AdvertisementItemWrapper.java
@@ -34,8 +34,12 @@ public class AdvertisementItemWrapper implements Comparable {
 
     @Override
     public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
 
         AdvertisementItemWrapper that = (AdvertisementItemWrapper) o;
 
