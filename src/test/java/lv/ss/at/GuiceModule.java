eadaa0c168bff011762fefdca7889f8f2baec8c6
Andrejs Cunskis <acunskis@evolutiongaming.com>
2017-10-26 21:24:01 +0300

    Set up driver once before test

--- src/test/java/lv/ss/at/GuiceModule.java
+++ src/test/java/lv/ss/at/GuiceModule.java
@@ -1,12 +1,19 @@
 package lv.ss.at;
 
 import com.google.inject.AbstractModule;
+import lv.ss.at.selenium.DriverImplementations;
 import lv.ss.at.selenium.DriverProvider;
 import org.openqa.selenium.WebDriver;
 
+import static java.lang.System.getProperty;
+
 public class GuiceModule extends AbstractModule {
+
     @Override
     public void configure() {
+        DriverImplementations
+                .valueOf(getProperty("webdriver", "chrome").toUpperCase())
+                .setUpDriver();
         bind(WebDriver.class).toProvider(DriverProvider.class);
     }
 }
--- src/test/java/lv/ss/at/selenium/DriverProvider.java
+++ src/test/java/lv/ss/at/selenium/DriverProvider.java
@@ -1,53 +1,16 @@
 package lv.ss.at.selenium;
 
 import com.google.inject.Provider;
-import io.github.bonigarcia.wdm.ChromeDriverManager;
-import io.github.bonigarcia.wdm.FirefoxDriverManager;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.firefox.FirefoxDriver;
-
-import java.util.Arrays;
 
 import static java.lang.System.getProperty;
-import static lv.ss.at.selenium.DriverProvider.DriverImplementations.getDriverImplementation;
 
 public class DriverProvider implements Provider<WebDriver> {
 
     @Override
     public WebDriver get() {
-        return getDriverImplementation(getProperty("webdriver", "chrome")).getDriver();
-    }
-
-    enum DriverImplementations {
-        CHROME("chrome") {
-            @Override
-            WebDriver getDriver() {
-                ChromeDriverManager.getInstance().setup();
-                return new ChromeDriver();
-            }
-        },
-        FIREFOX("firefox") {
-            @Override
-            WebDriver getDriver() {
-                FirefoxDriverManager.getInstance().setup();
-                return new FirefoxDriver();
-            }
-        };
-
-        String browser;
-
-        DriverImplementations(String browser) {
-            this.browser = browser;
-        }
-
-        abstract WebDriver getDriver();
-
-        static DriverImplementations getDriverImplementation(String browser) {
-            return Arrays.stream(values())
-                    .filter(driverImplementation -> driverImplementation.browser.equals(browser))
-                    .findFirst()
-                    .orElseThrow(() -> new RuntimeException("WebDriver not found!"));
-        }
+        return DriverImplementations
+                .valueOf(getProperty("webdriver", "chrome").toUpperCase())
+                .getWebDriver();
     }
 }
